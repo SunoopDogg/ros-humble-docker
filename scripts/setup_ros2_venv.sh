f4260a0c12bf8ec6cc6a6ecdaf93ff1556d5544d
SunoopDogg <aswoo55555@gmail.com>
2026-02-27 19:57:20 +0000

    refactor: Unify for Docker and Jetson environments

--- scripts/setup_ros2_venv.sh
+++ scripts/setup_ros2_venv.sh
@@ -6,10 +6,18 @@ PTH_FILE="ros2.pth"
 PYTHON_VERSION=$("$VENV_DIR/bin/python" -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 SITE_PACKAGES="$VENV_DIR/lib/python${PYTHON_VERSION}/site-packages"
 
+ROS_PREFIX="/opt/ros/${ROS_DISTRO}"
+
+if [ -d "$ROS_PREFIX/install" ]; then
+  ROS_PKG_PREFIX="$ROS_PREFIX/install"
+else
+  ROS_PKG_PREFIX="$ROS_PREFIX"
+fi
+
 cat > "$SITE_PACKAGES/$PTH_FILE" << EOL
 /usr/lib/python3/dist-packages
-/opt/ros/${ROS_DISTRO}/lib/python${PYTHON_VERSION}/site-packages
-/opt/ros/${ROS_DISTRO}/local/lib/python${PYTHON_VERSION}/dist-packages
+${ROS_PKG_PREFIX}/lib/python${PYTHON_VERSION}/site-packages
+${ROS_PKG_PREFIX}/local/lib/python${PYTHON_VERSION}/dist-packages
 EOL
 
 source "$VENV_DIR/bin/activate"
